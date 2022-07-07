333201451fa99007bf90f2530795c4c8dd3eaf06
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-07-07 11:23:44 -0300

    listandos repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,14 +2,6 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-// https://api.github.com/users/olucasfreitas/repos
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "http://github.com/unform/unform",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -24,10 +16,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return <RepositoryItem key={repository.id} repository={repository} />;
+        })}
       </ul>
     </section>
   );

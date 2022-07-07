c3e1df362dd371ec760204af6f0f9cc127e7a941
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-07-06 23:46:15 -0300

    passando repositório com props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = "unform";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: "http://github.com/unform/unform",
+};
 
 export function RepositoryList() {
   return (
@@ -6,18 +12,10 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">Acessar repositório</a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
